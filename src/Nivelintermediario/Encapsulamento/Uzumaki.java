0d84571c790f833522fc934463ad97f4d0418087
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2025-09-13 09:51:48 +0100

    aula sobre encapsulamento / Getters e Setters

---
+++ src/Nivelintermediario/Encapsulamento/Uzumaki.java
@@ -0,0 +1,10 @@
+package Nivelintermediario.Encapsulamento;
+
+public class Uzumaki extends Ninja {
+    public Uzumaki() {
+    }
+
+    public Uzumaki(String nome, String aldeia, int idade, int numeroDeMissoes, double altura) {
+        super(nome, aldeia, idade, numeroDeMissoes, altura);
+    }
+}
